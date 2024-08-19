de896f6ddc10da49fd47daca888cbd1478f1596d
gguzman89 <ggtpi0gabriel@gmail.com>
2024-08-19 00:06:03 +0000

    code example

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
-        console.log('cambio en el panel888');
+        console.log('cambio en el primavera');
         
     })
 })
